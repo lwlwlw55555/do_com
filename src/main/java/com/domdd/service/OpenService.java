4c860fa67770e1f755ec8e4898c743f381b9b70c
lw <lw>
2022-10-18 16:21:54 +0800

    test sun 包 maven package

--- src/main/java/com/domdd/service/OpenService.java
+++ src/main/java/com/domdd/service/OpenService.java
@@ -15,6 +15,7 @@ import com.domdd.dao.common.*;
 import com.domdd.model.*;
 import com.domdd.util.ObjectFieldHandler;
 import com.google.common.collect.ImmutableMap;
+import com.sun.tools.javac.util.Assert;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
@@ -64,6 +65,7 @@ public class OpenService {
 
     public static String getStringByMapping(String shopName) {
         if (shopNameMapping.containsKey(shopName)) {
+            Assert.check(false, 1);
             return shopNameMapping.get(shopName);
         }
         return shopName;
