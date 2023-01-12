690683ebc8123f32b215ad77dec5eda5929f399f
lw <lw>
2023-01-12 15:34:25 +0800

    add baseReq  ApiModelProperty

--- src/main/java/com/domdd/controller/req/OpenBaseReq.java
+++ src/main/java/com/domdd/controller/req/OpenBaseReq.java
@@ -1,5 +1,6 @@
 package com.domdd.controller.req;
 
+import io.swagger.annotations.ApiModelProperty;
 import io.swagger.annotations.ApiParam;
 import lombok.Data;
 
@@ -13,19 +14,25 @@ public class OpenBaseReq {
     @Min(value = 1, message = "页码必须是正整数")
     @Digits(integer = Long.SIZE, fraction = 0, message = "页码必须是正整数")
     @ApiParam(value = "页码", required = true, example = "1")
+    @ApiModelProperty(value = "页码")
     private Integer page = 1;
 
     @Digits(integer = Long.SIZE, fraction = 0, message = "页面大小必须是正整数")
     @ApiParam(value = "每页记录数", required = true, example = "10")
+    @ApiModelProperty(value = "每页记录数")
     @Max(100)
     private Integer pageSize = 100;
 
     @NotBlank(message = "appkey is null")
+    @ApiModelProperty("appkey")
     private String appkey;
     @NotBlank(message = "sign is null")
+    @ApiModelProperty("签名")
     private String sign;
     @NotBlank(message = "timestamp is null")
+    @ApiModelProperty("时间戳")
     private String timestamp;
     @NotBlank(message = "method is null")
+    @ApiModelProperty("接口类型 order.list purchaseInOrder.list afterSaleOrder.list afterSaleReturnOrder.list inventory.list")
     private String method;
 }
