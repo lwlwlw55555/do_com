107a3251f24ef11aafc91e3a14d7ebdbdb4f6058
lw <lw>
2023-02-16 18:28:09 +0800

    [fix] fix 退货单 group quantity

--- src/main/java/com/domdd/dao/common/AfterSaleOrderMapper.java
+++ src/main/java/com/domdd/dao/common/AfterSaleOrderMapper.java
@@ -14,13 +14,15 @@ import java.util.List;
 public interface AfterSaleOrderMapper extends BaseMapper<AfterSaleOrder> {
     default IPage<AfterSaleOrder> selectByPage(IPage<AfterSaleOrder> page, Date startTime, Date endTime, String shopName) {
         LambdaQueryWrapper<AfterSaleOrder> wrapper = new LambdaQueryWrapper<>();
-        wrapper.eq(AfterSaleOrder::getShopName, shopName);
+//        wrapper.eq(AfterSaleOrder::getShopName, shopName);
         wrapper.ge(AfterSaleOrder::getLastUpdatedTime, startTime)
                 .lt(AfterSaleOrder::getLastUpdatedTime, endTime)
                 .eq(AfterSaleOrder::getAfterSalesStatus, 10)
                 .eq(AfterSaleOrder::getReturnStatus, "DONE")
+                // todo 废弃退款单
+                .eq(AfterSaleOrder::getShopName, "LW_TEST")
 //                .ge(AfterSaleOrder::getLastUpdatedTime, DateUtil.parseDate("2023-02-01 00:00:00"))
-                .last(" and false")
+//                .last(" and 1=0")
 //                .isNotNull(AfterSaleOrder::getRefundId);
         ;
         return this.selectPage(page, wrapper);
--- src/main/java/com/domdd/dao/common/AfterSaleReturnOrderMapper.java
+++ src/main/java/com/domdd/dao/common/AfterSaleReturnOrderMapper.java
@@ -12,19 +12,20 @@ import java.util.Date;
 import java.util.List;
 
 public interface AfterSaleReturnOrderMapper extends BaseMapper<AfterSaleReturnOrder> {
-    default IPage<AfterSaleReturnOrder> selectByPage(IPage<AfterSaleReturnOrder> page, Date startTime, Date endTime, String shopName) {
-        LambdaQueryWrapper<AfterSaleReturnOrder> wrapper = new LambdaQueryWrapper<>();
-        wrapper.eq(AfterSaleReturnOrder::getShopName, shopName);
-        wrapper.ge(AfterSaleReturnOrder::getLastUpdatedTime, startTime)
-                .lt(AfterSaleReturnOrder::getLastUpdatedTime, endTime)
-                .eq(AfterSaleReturnOrder::getAfterSalesStatus, 10)
-                .eq(AfterSaleReturnOrder::getReturnStatus, "DONE")
-                .in(AfterSaleReturnOrder::getWarehouseId, 111, 112, 113, 1084, 1085, 1086)
-//                .ge(AfterSaleReturnOrder::getLastUpdatedTime, DateUtil.parseDate("2023-02-01 00:00:00"))
-//                .isNotNull(AfterSaleReturnOrder::getRefundId);
-        ;
-        return this.selectPage(page, wrapper);
-    }
+    //    default IPage<AfterSaleReturnOrder> selectByPage(IPage<AfterSaleReturnOrder> page, Date startTime, Date endTime, String shopName) {
+//        LambdaQueryWrapper<AfterSaleReturnOrder> wrapper = new LambdaQueryWrapper<>();
+//        wrapper.eq(AfterSaleReturnOrder::getShopName, shopName);
+//        wrapper.ge(AfterSaleReturnOrder::getLastUpdatedTime, startTime)
+//                .lt(AfterSaleReturnOrder::getLastUpdatedTime, endTime)
+//                .eq(AfterSaleReturnOrder::getAfterSalesStatus, 10)
+//                .eq(AfterSaleReturnOrder::getReturnStatus, "DONE")
+//                .in(AfterSaleReturnOrder::getWarehouseId, 111, 112, 113, 1084, 1085, 1086)
+////                .ge(AfterSaleReturnOrder::getLastUpdatedTime, DateUtil.parseDate("2023-02-01 00:00:00"))
+////                .isNotNull(AfterSaleReturnOrder::getRefundId);
+//        ;
+//        return this.selectPage(page, wrapper);
+//    }
+    IPage<AfterSaleReturnOrder> selectByPage(IPage<AfterSaleReturnOrder> page, @Param("startTime") Date startTime, @Param("endTime") Date endTime, @Param("shopName") String shopName);
 
     void replaceBatch(@Param("records") List<AfterSaleReturnOrder> records);
 }
